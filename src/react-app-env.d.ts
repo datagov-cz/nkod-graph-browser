c4d19af2b1da51169e8017fced62c7a017698b06
Štěpán Stenchlák <s.stenchlak@gmail.com>
2022-06-02 23:10:32 +0300

    env type definitions

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    /**
+     * SPARQL endpoint URL to obtain the dataset info
+     */
+    REACT_APP_DATASETS_SPARQL_URL: string;
+    /**
+     * URL to SGOV ShowIt browser
+     */
+    REACT_APP_SHOW_IT_URL: string;
+    /**
+     * SPARQL endpoint URL to obtain information about SGOV entities
+     */
+    REACT_APP_SGOV_SPARQL_URL: string;
+  }
+}
